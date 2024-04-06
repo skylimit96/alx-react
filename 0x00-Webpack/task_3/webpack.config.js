97ea4eae8c3fadf42a994a24c2a343d2d0e477e0
skylimit96 <a.sehlaoui96@gmail.com>
2024-04-06 00:53:57 +0000

    task 3

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,13 +1,19 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: {
     main: path.resolve(__dirname, "./js/dashboard_main.js"),
   },
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
+  },
+  devServer: {
+	port: 8564,
+	contentBase: path.join(__dirname, "public"),
+	compress: true,
+	open: true,
   },
   performance: {
     hints: false,
