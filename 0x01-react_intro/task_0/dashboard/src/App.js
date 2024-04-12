a322cb8f1e0ba5021ca30f1f12ca8ca7e190c2cf
Amine Sehlaoui <83908118+skylimit96@users.noreply.github.com>
2024-04-12 00:01:25 +0000

    Add files via upload

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import logo from './holberton-logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-header">
+        <img src={logo} alt="logo" />
         <h1>School dashboard</h1>
-      </header>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
       </div>
-      <footer className="App-footer">
+      <div className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+      </div>
+      <div className="App-footer">
         <p>Copyright 2020 - holberton School</p>
-      </footer>
+      </div>
     </div>
   );
 }
